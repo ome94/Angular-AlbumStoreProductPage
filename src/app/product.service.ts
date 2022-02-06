379a816e976d9ba04608712ebf5f451fdddf7fd6
Mayowa Emmanuel Olabanji <mayowae@outlook.com>
2022-02-06 03:39:20 +0100

    Define Product Service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import {Http, Response } from '@angular/http';
+
+import 'rxjs/add/operator';
 
 @Injectable()
 export class ProductService {
 
-  constructor() { }
+  _albumUrl = '../assets/album.json';
+
+  constructor(private _http: Http) { }
+
+  getAlbum() {
+    return this._http.get(this._albumUrl).pipe(
+      map(product => product.json())
+    );
+  }
 
 }
